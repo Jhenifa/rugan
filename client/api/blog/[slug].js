710231eb93179635716ff3e8735b71639cff1584
sheddy-cyber <idikechinaza@gmail.com>
2026-05-18 11:13:34 +0100

    modify seo behaviour

--- client/api/blog/[slug].js
+++ client/api/blog/[slug].js
@@ -109,9 +109,7 @@ function buildGlobalHeadExtras(origin) {
     '<meta name="viewport" content="width=device-width, initial-scale=1.0" />',
     '<meta name="theme-color" content="#4F7B44" />',
     '<meta name="format-detection" content="telephone=yes" />',
-    '<link rel="manifest" href="/site.webmanifest" />',
     '<link rel="icon" type="image/jpeg" href="/icons/favicon.jpg" />',
-    '<link rel="apple-touch-icon" href="/icons/favicon.jpg" />',
     `<link rel="sitemap" type="application/xml" title="Sitemap" href="${escapeHtml(
       `${origin}/sitemap.xml`,
     )}" />`,
